945271d4aec984451ca1fc1ef4ab571d41d94497
Shaaaaaaat <109889828+Shaaaaaaat@users.noreply.github.com>
2024-08-25 15:16:21 +0200

    Ваше описание изменений

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const messages = loadMessages();
 
 // Функция для генерации уникального ID в допустимом диапазоне
 function generateUniqueId() {
-  const maxId = 2147483647; // Максимально допустимое значение
-  const minId = 1; // Минимально допустимое значение
+  const maxId = 2147483647;
+  const minId = 1;
   return (Date.now() % (maxId - minId + 1)) + minId;
 }
 
@@ -47,9 +47,24 @@ async function createPaymentLink(priceId, email) {
         quantity: 1,
       },
     ],
-    customer_email: email, // Добавление email клиента
+    customer_email: email,
   });
   return paymentLink.url;
+}
+
+// Функция для генерации ссылки на оплату через Робокассу
+function generatePaymentLinkRobokassa(paymentId, amount, email) {
+  const shopId = process.env.ROBO_ID;
+  const secretKey1 = process.env.ROBO_SECRET1;
+
+  const signature = crypto
+    .createHash("md5")
+    .update(`${shopId}:${amount}:${paymentId}:${secretKey1}`)
+    .digest("hex");
+
+  return `https://auth.robokassa.ru/Merchant/Index.aspx?MerchantLogin=${shopId}&OutSum=${amount}&InvId=${paymentId}&SignatureValue=${signature}&Email=${encodeURIComponent(
+    email
+  )}&IsTest=0`;
 }
 
 // Функция для отправки данных в Airtable
@@ -146,7 +161,11 @@ bot.on("callback_query:data", async (ctx) => {
     await session.save();
 
     if (action === "rubles") {
-      const paymentLink = generatePaymentLink(paymentId, session.email);
+      const paymentLink = generatePaymentLinkRobokassa(
+        paymentId,
+        3,
+        session.email
+      );
       await ctx.reply(
         `Отправляю ссылку для оплаты в рублях. Пройдите, пожалуйста, по ссылке: ${paymentLink}`
       );
@@ -158,6 +177,7 @@ bot.on("callback_query:data", async (ctx) => {
           `Отправляю ссылку для оплаты в евро. Пройдите, пожалуйста, по ссылке: ${paymentLink}`
         );
       } catch (error) {
+        console.error("Stripe error:", error);
         await ctx.reply(
           "Произошла ошибка при создании ссылки для оплаты. Попробуйте снова позже."
         );
